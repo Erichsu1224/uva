07cf8076ec9c3cdb8e050a32ae0b77f3b00ee2b6
Eric <40513062+Erichsu1224@users.noreply.github.com>
2019-05-07 16:34:12 +0800

    5/9

---
+++ uva1169.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+#define ll long long
+#define C cases
+#define PB push_back
+#define PP pair<int, int>
+
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	#ifndef file
+	freopen("in.in", "r", stdin);
+	freopen("out.out", "w", stdout);
+	#endif
+
+	
+
+	return 0;
+}
